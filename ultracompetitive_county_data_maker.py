457e04d6bc2a3fdc46e1bb2ca7e3e0ef65afd9d8
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2021-07-17 14:11:44 -0400

    adds county level covariates to the data set

--- ultracompetitive_county_data_maker.py
+++ ultracompetitive_county_data_maker.py
@@ -13,8 +13,8 @@ filepath = 'C:/Users/' + username + '/Documents/Data/ultracompetitive/'
 
 compdata = pd.read_csv(filepath + 'output.csv')
 edu = pd.read_csv(filepath + 'Education.csv', engine = 'python')
-pop = pd.read_csv(filepath + 'PopulationEstimates.csv', engine = 'python')
-unemp = pd.read_csv(filepath + 'Unemployment.csv', engine = 'python')
+labor = pd.read_csv(filepath + 'Unemployment.csv', engine = 'python')
+inc = pd.read_csv(filepath + 'Income.csv', engine = 'python')
 ccmap = pd.read_csv(filepath + 'ccmap.csv', sep = '|')
 cases = pd.read_csv(filepath + 'time_series_covid19_confirmed_US.csv')
 
@@ -32,171 +32,171 @@ cases = pd.concat([cases, lows], axis = 1)
 # Defining a helper function for assigning FIPS via ccmap and cases
 
 def flipsadelphia(inp):
-    
+   
     city = inp.City
     state = inp.State.upper().strip('"')
     sx = list(ccmap['State short']).index(state)
     st = ccmap['State full'][sx]
-    
+   
     try:
-        
+       
         cc = ccmap[ccmap['City'] == city]
         cc = cc[cc['State short'] == state]
         county = cc.iloc[0]['County'].lower()
-        
+       
     except:
-        
+       
         county = 'NOPE'
-    
+   
     if county != 'NOPE':
-        
+       
         if (county[0:5] == 'saint') and (county != 'saint marys'):
-            
+           
             back = county[5:]
             county = 'st.' + back
-        
+       
         elif county == 'virginia beach city':
-            
+           
             county = 'virginia beach'
-            
+           
         elif county ==  'alexandria city':
-            
+           
             county = 'alexandria'
-            
+           
         elif county == 'norfolk city':
-            
+           
             county = 'norfolk'
-            
+           
         elif county == 'fredericksburg city':
-            
+           
             county = 'fredericksburg'
-            
+           
         elif county == 'chesapeake city':
-            
+           
             county = 'chesapeake'
-        
+       
         elif county == 'lexington city':
-            
+           
             county = 'lexington'
-            
+           
         elif county == 'falls church city':
-            
+           
             county = 'falls church'
-            
+           
         elif county == 'staunton city':
-            
+           
             county = 'staunton'
-            
+           
         elif county == 'la porte':
-            
+           
             county = 'laporte'
-            
+           
         elif county == 'suffolk city':
-            
+           
             county = 'suffolk'
-            
+           
         elif county == 'newport news city':
-            
+           
             county = 'newport news'
-            
+           
         elif county == 'hampton city':
-            
+           
             county = 'hampton'
-        
+       
         elif county == 'manassas city':
-            
+           
             county = 'manassas'
-            
+           
         elif county == 'harrisonburg city':
-            
+           
             county = 'harrisonburg'
-            
+           
         elif county == 'prince georges':
-            
+           
             county = "prince george's"
-            
+           
         elif county == 'la salle':
-            
+           
             county = 'lasalle'
-            
+           
         elif county == 'saint marys':
-            
+           
             county = "st. mary's"
-            
+           
         elif county == 'lynchburg city':
-            
+           
             county = 'lynchburg'
-            
+           
         elif county == 'portsmouth city':
-            
+           
             county = 'portsmouth'
-            
+           
         elif county == 'poquoson city':
-            
+           
             county = 'poquoson'
-            
+           
         elif county == 'queen annes':
-            
+           
             county = "queen anne's"
-            
+           
         elif county == 'matanuska susitna':
-            
+           
             county = 'matanuska-susitna'
-            
+           
         elif county == 'st joseph':
-            
+           
             county = 'st. joseph'
-            
+           
         elif county == 'de kalb':
-            
+           
             county = 'dekalb'
-            
+           
         elif county == 'waynesboro city':
-            
+           
             county = 'waynesboro'
-            
+           
         elif county == 'winchester city':
-            
+           
             county = 'winchester'
-            
+           
         elif county == 'martinsville city':
-            
+           
             county = 'martinsville'
-            
+           
         elif county == 'danville city':
-            
+           
             county = 'danville'
-            
+           
         elif county == 'bristol city':
-            
+           
             county = 'bristol'
-            
+           
         elif county == 'de witt':
-            
+           
             county = 'dewitt'
-            
+           
         elif county == 'galax city':
-            
+           
             county = 'galax'
-            
+           
         elif county == 'colonial heights city':
-            
+           
             county = 'colonial heights'
-        
+       
         try:
-            
+           
             tmp = cases[cases['County'] == county]
             tmp = tmp[tmp['Province_State'] == st]
             flips = int(tmp.iloc[0]['FIPS'])
-        
+       
         except:
-            
+           
             flips = None
-        
+       
     else:
-        
+       
         flips = None
-        
+       
     return flips
 
 # Using the function to get FIPS codes
@@ -204,9 +204,9 @@ def flipsadelphia(inp):
 fips = []
 
 for i in range(len(compdata)):
-    
+   
     print(str(i+1) + ' of ' + str(len(compdata)) + '.......') # Visualize progress
-    
+   
     fips.append(flipsadelphia(compdata.iloc[i]))
 
 # Adding FIPS codes to the main dataframe
@@ -218,78 +218,101 @@ compdata = pd.concat([compdata, fips], axis = 1)
 
 # Education data
 
-edu_cc = []
-edu_bs = []
+ed_cols = ['percentsomehs_', 'percenthsgrad_', 'percentsomecollege_', 'percentassociates_', 'percentbachelors_', 'percentgrad_degree_'] # base of column headings
+
+somehs = []
+hs = []
+someuni = []
+ass = []
+bach = []
+grad = []
 
 for i in range(len(compdata)):
     
-    print(str(i+1) + ' of ' + str(len(compdata)) + '.......') # Visualize progress
+    print('Education :: ' +  str(i+1) + ' of ' + str(len(compdata)) + '.......') # Visualize progress
     
     try:
         
-        fip = compdata.iloc[i]['FIPS']
-        tmp = edu[edu['FIPS Code'] == fip]
-        edu_cc.append(tmp.iloc[0]["Percent of adults completing some college or associate's degree, 2015-19"])
-        edu_bs.append(tmp.iloc[0]["Percent of adults with a bachelor's degree or higher, 2015-19"])
+        fip = compdata.iloc[i]['FIPS'] # Get the location of the event
+        yr = compdata.iloc[i]['RACE_Year'] # Get the year of the event
+        
+        tmp = edu[edu['ï»¿fips'] == fip].reset_index(drop = True) # Subset for location
+        
+        somehs.append(tmp[ed_cols[0] + str(yr)][0])
+        hs.append(tmp[ed_cols[1] + str(yr)][0])
+        someuni.append(tmp[ed_cols[2] + str(yr)][0])
+        ass.append(tmp[ed_cols[3] + str(yr)][0])
+        bach.append(tmp[ed_cols[4] + str(yr)][0])
+        grad.append(tmp[ed_cols[5] + str(yr)][0])
         
     except:
         
-        edu_cc.append(None)
-        edu_bs.append(None)
+        somehs.append(None)
+        hs.append(None)
+        someuni.append(None)
+        ass.append(None)
+        bach.append(None)
+        grad.append(None)
 
-edu_cc = pd.Series(edu_cc, name = 'Some_College_Ass')
-edu_bs = pd.Series(edu_bs, name = 'College_Degree')
-compdata = pd.concat([compdata, edu_cc, edu_bs], axis = 1)
+somehs = pd.Series(somehs, name = 'Some_HS')
+hs = pd.Series(hs, name = 'HS')
+someuni = pd.Series(someuni, name = 'Some_Uni')
+ass = pd.Series(ass, name = 'Associate')
+bach = pd.Series(bach, name = 'Bachelor')
+grad = pd.Series(grad, name = 'Graduate')
 
-# Population data
+compdata = pd.concat([compdata, somehs, hs, someuni, ass, bach, grad], axis = 1)
 
-popu = []
+# Unemployment data
+
+unemp = []
 
 for i in range(len(compdata)):
-    
-    print(str(i+1) + ' of ' + str(len(compdata)) + '.......') # Visualize progress
-    
+   
+    print('Unemployment :: ' + str(i+1) + ' of ' + str(len(compdata)) + '.......') # Visualize progress
+   
     try:
-        
-        fip = compdata.iloc[i]['FIPS']
-        tmp = pop[pop['FIPStxt'] == fip]
-        popu.append(tmp.iloc[0]['POP_ESTIMATE_2019'])
-                
+       
+        fip = compdata.iloc[i]['FIPS'] # Get the location of the event
+        yr = compdata.iloc[i]['RACE_Year'] # Get the year of the event
+       
+        tmp = labor[labor['ï»¿fips'] == fip] # Subset for location
+        tmp = tmp[tmp.year == yr].reset_index(drop = True) # Subset for year
+       
+        unemp.append(tmp.unemploymentrate[0])
+       
     except:
-        
-        popu.append(None)
+       
+        unemp.append(None)
 
-popu = pd.Series(popu, name = 'County_Population')
-compdata = pd.concat([compdata, popu], axis = 1)
+unemp = pd.Series(unemp, name = 'Unemployment_Rate')
+compdata = pd.concat([compdata, unemp], axis = 1)
 
-# Unemployment and Income data
+# Income data
 
-nojo = []
 hhinc = []
 
 for i in range(len(compdata)):
-    
-    print(str(i+1) + ' of ' + str(len(compdata)) + '.......') # Visualize progress
-    
+   
+    print('Income :: ' + str(i+1) + ' of ' + str(len(compdata)) + '.......') # Visualize progress
+   
     try:
-        
-        fip = compdata.iloc[i]['FIPS']
-        tmp = unemp[unemp['fips_txt'] == fip]
-        tmpu = tmp[tmp['Attribute'] == 'Unemployment_rate_2019']
-        tmpi = tmp[tmp['Attribute'] == 'Median_Household_Income_2019']
-        nojo.append(tmpu.iloc[0]['Value'])
-        hhinc.append(tmpi.iloc[0]['Value'])
-        
+       
+        fip = compdata.iloc[i]['FIPS'] # Get the location of the event
+        yr = compdata.iloc[i]['RACE_Year'] # Get the year of the event
+       
+        tmp = inc[inc['countyid'] == fip] # Subset for location
+        tmp = tmp[tmp['ï»¿year'] == yr].reset_index(drop = True) # Subset for year
+       
+        hhinc.append(tmp.medianhouseholdincome[0])
+       
     except:
-        
-        nojo.append(None)
+       
         hhinc.append(None)
 
-nojo = pd.Series(nojo, name = 'Unemployment_Rate')
 hhinc = pd.Series(hhinc, name = 'Median_Household_Income')
-compdata = pd.concat([compdata, nojo, hhinc], axis = 1)
+compdata = pd.concat([compdata, hhinc], axis = 1)
 
 # Write dataframe to file
 
 compdata.to_csv(filepath + 'output.csv', index = False)
-
