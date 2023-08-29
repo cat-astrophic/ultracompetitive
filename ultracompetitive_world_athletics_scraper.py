f7149ee3140517a7af6205236a84030a7710f6bd
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2023-08-29 15:17:27 -0400

    scrapes world athletics

--- ultracompetitive_world_athletics_scraper.py
+++ ultracompetitive_world_athletics_scraper.py
@@ -57,6 +57,43 @@ for event in event_list:
                     
                     continue
 
+# Adding sprints
+
+sprint_list = ['100-metres', '200-metres', '400-metres']
+
+for event in sprint_list:
+    
+    for gender in gender_list:
+        
+        for year in range(2001,2023):
+            
+            for p in range(1,21):
+                
+                print(event + ' :: ' + gender + ' :: ' + str(year) + ' :: ' + str(p))
+                
+                try:
+                    
+                    url = 'https://worldathletics.org/records/toplists/sprints/' + event + '/outdoor/' + gender + '/senior/' + str(year) + '?regionType=world&timing=electronic&page=' + str(p) + '&bestResultsOnly=false'
+                    page = urllib.request.Request(url, headers = {'User-Agent': 'Mozilla/5.0'})
+                    response = urllib.request.urlopen(page)
+                    soup = bs(response, 'html.parser')
+                    data = soup.find_all('td')
+                    x = len(data) / 100
+                    
+                    for i in range(100):
+                        
+                        ranks.append(str(data[int(x*i)])[20:-5].replace(' ', ''))
+                        marks.append(str(data[int(x*i) + 1])[20:].replace(' ', '').replace('\n</td>', ''))
+                        names.append(str(data[int(x*i) + 2])[53:str(data[int(x*i) + 2])[53:].find('\n')+51])
+                        nats.append(str(data[int(x*i) + 4])[29:32])
+                        years.append(year)
+                        genders.append(gender)
+                        events.append(event)
+                        
+                except:
+                    
+                    continue
+
 # Make a dataframe
 
 ranks = pd.Series(ranks, name = 'Rank')
@@ -68,6 +105,38 @@ genders = pd.Series(genders, name = 'Gender')
 events = pd.Series(events, name = 'Event')
 
 df = pd.concat([years, genders, events, ranks, marks, names, nats], axis = 1)
+
+# Clean the runner column
+
+dfrnr = [str(x)[-8:] for x in df.Runner]
+df.Runner = dfrnr
+
+# Adding a column for seconds taken to complete race
+
+def secsy(row):
+    
+    try:
+        
+        if row.Event != 'marathon':
+            
+            raw = str(row.Mark).replace('h', '0')
+            idx = raw.index(':')
+            shit = 60*int(raw[:idx]) + int(raw[-5:-3]) + int(raw[-2:])/100
+            
+        else:
+            
+            raw = str(row.Mark).replace('h', '0')
+            shit = 3600*int(raw[:1]) + 60*int(raw[2:4]) + int(raw[-2:])
+            
+    except:
+        
+        shit = None
+        
+    return shit
+
+secs = [secsy(df.iloc[i]) for i in range(len(df))]
+
+df = pd.concat([df, pd.Series(secs, name = 'Seconds')], axis = 1)
 
 # Save the dataframe
 
