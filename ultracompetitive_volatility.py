4feaf2a2629e5c4c3b0573f32ce1d071fcbc0201
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2024-03-05 22:25:00 -0500

    preps data for the volatility analyses

--- ultracompetitive_volatility.py
+++ ultracompetitive_volatility.py
@@ -13,6 +13,10 @@ direc = 'D:/ultracompetitive/'
 
 data = pd.read_csv(direc + 'output.csv')
 
+# Drop events measured in distance
+
+data = data[np.isnan(data.Seconds) == False].reset_index(drop = True)
+
 # Creating a relative place variable for each observation
 
 rp = []
@@ -24,7 +28,7 @@ for i in range(len(data)):
     tmp = data[data.RACE_ID == data.RACE_ID[i]]
     tmp = tmp[tmp.Gender == data.Gender[i]]
     
-    rp.append(data.Gender_Place[i] / len(tmp))
+    rp.append(1 - ((data.Gender_Place[i] - 1) / len(tmp)))
 
 data = pd.concat([data, pd.Series(rp, name = 'Relative_Place')], axis = 1)
 
@@ -52,13 +56,62 @@ for r in data.Runner_ID.unique():
 
 rp_data = pd.concat([pd.Series(data.Runner_ID.unique(), name = 'Runner'), pd.Series(genders, name = 'Gender'), pd.Series(counts, name = 'Races'), pd.Series(volatility, name = 'Volatility'), pd.Series(mean_rp, name = 'Ability')], axis = 1)
 
+# Add runner characteristics to rp_data
+
+age = [] # Age in 2021
+county = [] # Most recent location
+state = []
+altitude = [] # Mean
+some_hs = []
+hs = []
+som_uni = []
+ass = []
+bach = []
+grad = []
+ur = []
+mhhi = []
+
+for r in data.Runner_ID.unique():
+    
+    print('Computing relative place for runner ' + str(list(data.Runner_ID.unique()).index(r)+1) + ' of ' + str(len(data.Runner_ID.unique())) + '.......')
+    
+    tmp = data[data.Runner_ID == r].reset_index(drop = True)
+    
+    age.append(tmp.Age[0] + 2021 - tmp.RACE_Year[0])
+    county.append(tmp.Runner_County[0])
+    state.append(tmp.State[0])
+    altitude.append(np.mean(tmp.Altitude))
+    some_hs.append(np.mean(tmp.Some_HS))
+    hs.append(np.mean(tmp.HS))
+    som_uni.append(np.mean(tmp.Some_Uni))
+    ass.append(np.mean(tmp.Associate))
+    bach.append(np.mean(tmp.Bachelor))
+    grad.append(np.mean(tmp.Graduate))
+    ur.append(np.mean(tmp.Unemployment_Rate))
+    mhhi.append(np.mean(tmp.Median_Household_Income))
+
+age = pd.Series(age, name = 'Age')
+county = pd.Series(county, name = 'County')
+state = pd.Series(state, name = 'State')
+altitude = pd.Series(altitude, name = 'Altitude')
+some_hs = pd.Series(some_hs, name = 'Some_HS')
+hs = pd.Series(hs, name = 'HS')
+som_uni = pd.Series(som_uni, name = 'Some_Uni')
+ass = pd.Series(ass, name = 'Associate')
+bach = pd.Series(bach, name = 'Bachelor')
+grad = pd.Series(grad, name = 'Graduate')
+ur = pd.Series(ur, name = 'Unemployment_Rate')
+mhhi = pd.Series(mhhi, name = 'Median_Household_Income')
+
+rp_data = pd.concat([rp_data, age, county, state, altitude, some_hs, hs, som_uni, ass, bach, grad, ur, mhhi], axis = 1)
+
 # Save rp_data
 
 rp_data.to_csv(direc + 'volatility_data.csv', index = False)
 
 # Removing people who do not race frequently enough to consider in this analysis
 
-rpx = rp_data[rp_data.Races >= 10]
+rpx = rp_data[rp_data.Races >= 5]
 
 # Removing people who are not in the top 25% of runners, typically
 
